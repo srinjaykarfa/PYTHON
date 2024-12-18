c7ab871093142d06dabf3fa93017a6197dd606b7
Srinjay Karfa <srinjay.karfa.official@gmail.com>
2024-12-18 12:58:19 +0530

    Initial Commit

--- Chapter 1 - PS/Problem3.py
+++
@@ -1,4 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("I am Srinjay Karfa")
-engine.runAndWait()
--- Chapter 4 -PS/Problem2.py
+++
@@ -1,5 +0,0 @@
-marks=[]
-for i in range(1,7):
-    marks.append(input(f"Marks of {i} is: "))
-marks.sort()
-print(marks)
